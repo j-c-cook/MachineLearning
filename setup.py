93f1af666e405798a1be5ee48c2d93c2234d3608
j-c-cook <jack.cook@okstate.edu>
2020-09-08 12:23:35 -0500

    setup package and requirements for running

---
+++ setup.py
@@ -0,0 +1,26 @@
+# Jack C. Cook
+# Tuesday, September 8, 2020
+
+from setuptools import setup
+
+
+def getreqs(fname):
+    """
+    Get the requirements list from the text file
+    JCC 03.10.2020
+    :param fname: the name of the requirements text file
+    :return: a list of requirements
+    """
+    file = open(fname)
+    data = file.readlines()
+    file.close()
+    return [data[i].replace('\n', '') for i in range(len(data))]
+
+
+setup(name='MachineLearning',
+      version='0.0.1',
+      packages=['MachineLearning'],
+      install_requires=getreqs('requirements.txt'),
+      author='Jack C. Cook',
+      author_email='jack.cook@okstate.edu',
+      description='A package containing the scripts developed in CS 5783 at Oklahoma State University.')
